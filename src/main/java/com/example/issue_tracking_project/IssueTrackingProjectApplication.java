627ed2ea76876c112e9b6be9c31edf9e387425f4
Ashwini <gourashwini115@gmail.com>
2025-01-27 22:41:34 -0500

    FireBase code added

--- src/main/java/com/example/issue_tracking_project/IssueTrackingProjectApplication.java
+++ src/main/java/com/example/issue_tracking_project/IssueTrackingProjectApplication.java
@@ -1,13 +1,46 @@
 package com.example.issue_tracking_project;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class IssueTrackingProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IssueTrackingProjectApplication.class, args);
-	}
+    
+
+    
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = IssueTrackingProjectApplication.class.getClassLoader();
+        File file = new File(classLoader.getResource("serviceAccountKey.json").getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+        if (!FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.getInstance().delete();
+            System.out.println("Removed existing FirebaseApp instance.");
+        }
+    
+
+        FirebaseOptions options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl("https://issue-tracking-db-default-rtdb.firebaseio.com/") // Your database URL
+        .build();
+        
+        FirebaseApp.initializeApp(options);
+
+        // FirebaseApp myApp = FirebaseApp.initializeApp(getApplicationContext(),options,
+        // "MyAppName");
+
+        SpringApplication.run(IssueTrackingProjectApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/issue_tracking_project_model/TicketModel.java
+++ src/main/java/com/example/issue_tracking_project_model/TicketModel.java
@@ -5,48 +5,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class TicketModel {
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String description;
-    private String status;
-    private String assignedTo;
+    private String sStatus;
+    private String sAssigned;
+    private String sSupervisor;
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getAssignedTo() {
-        return assignedTo;
-    }
-
-    public void setAssignedTo(String assignedTo) {
-        this.assignedTo = assignedTo;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  
 }
