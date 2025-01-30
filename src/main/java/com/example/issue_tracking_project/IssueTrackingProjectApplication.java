f93b96cf2b989ebf06f31ff30b2189ba2ae530b3
Ashwini <gourashwini115@gmail.com>
2025-01-29 20:10:47 -0500

    Removing Firebase and adding JPA

--- src/main/java/com/example/issue_tracking_project/IssueTrackingProjectApplication.java
+++ src/main/java/com/example/issue_tracking_project/IssueTrackingProjectApplication.java
@@ -18,22 +18,22 @@ public class IssueTrackingProjectApplication {
 
     
     public static void main(String[] args) throws IOException {
-        ClassLoader classLoader = IssueTrackingProjectApplication.class.getClassLoader();
-        File file = new File(classLoader.getResource("serviceAccountKey.json").getFile());
-        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        // ClassLoader classLoader = IssueTrackingProjectApplication.class.getClassLoader();
+        // File file = new File(classLoader.getResource("serviceAccountKey.json").getFile());
+        // FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
 
-        if (!FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.getInstance().delete();
-            System.out.println("Removed existing FirebaseApp instance.");
-        }
+        // if (!FirebaseApp.getApps().isEmpty()) {
+        //     FirebaseApp.getInstance().delete();
+        //     System.out.println("Removed existing FirebaseApp instance.");
+        // }
     
 
-        FirebaseOptions options = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-        .setDatabaseUrl("https://issue-tracking-db-default-rtdb.firebaseio.com/")
-        .build();
+        // FirebaseOptions options = FirebaseOptions.builder()
+        // .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        // .setDatabaseUrl("https://issue-tracking-db-default-rtdb.firebaseio.com/")
+        // .build();
         
-        FirebaseApp.initializeApp(options);
+        // FirebaseApp.initializeApp(options);
 
         SpringApplication.run(IssueTrackingProjectApplication.class, args);
     }
