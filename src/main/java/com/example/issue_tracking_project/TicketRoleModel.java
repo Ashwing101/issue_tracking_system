c14c0801e9631edfd8ad9caa14ba13208d672cd4
Ashwini <gourashwini115@gmail.com>
2025-02-10 23:45:14 -0500

    New Model added

--- src/main/java/com/example/issue_tracking_project/TicketRoleModel.java
+++ src/main/java/com/example/issue_tracking_project/TicketRoleModel.java
@@ -1,24 +1,35 @@
 package com.example.issue_tracking_project;
 
 
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
+@Table(name = "tb_roles", schema = "db_issue_tracking")
 public class TicketRoleModel {
 
+@Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private int Id;
-@Id
+
 private String RoleId;
 private String RoleName;
 private String RoleDescription;
 private String RoleType;
+
+@OneToMany(mappedBy = "TicketRoleModel")
+private List<UserDetails> users;
 
 }
--- src/main/java/com/example/issue_tracking_project/TicketUserModel.java
+++ src/main/java/com/example/issue_tracking_project/TicketUserModel.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "tb_user_details", schema = "db_issue_tracking")
 public class TicketUserModel {
 
 @Id
